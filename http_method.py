eecbfc5d49e1b374124a63fc83e7a5de631934d2
huhumt <nepood@gmail.com>
2021-02-20 16:13:48 +0800

    update in 2021.02.20

--- http_method.py
+++ http_method.py
@@ -29,7 +29,9 @@ if __name__ == "__main__":
     This is for test
     """
 
-    content = http_get("https://blog.csdn.net/imnisen1992/article/details/53165112")
-    print(content)
-    content = http_get("https://blog.csdn.net/jcjc918")
+    # content = http_get("https://blog.csdn.net/imnisen1992/article/details/53165112")
+    # print(content)
+    # content = http_get("https://blog.csdn.net/jcjc918")
+    # print(content)
+    content = http_get("https://yinwang1.wordpress.com/author/yinwang0/")
     print(content)
